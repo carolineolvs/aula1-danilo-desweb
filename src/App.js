f8c594d3181431b8914d9a69e0e6540952890f14
carolineolvs <carolineolvs0@gmail.com>
2025-10-28 15:55:27 -0300

    Desafio 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
+//import de estilos
 import './App.css';
-// import do componente
+
+//import do componente
 import FirstComponent from './components/FirstComponent';
 import TemplateExpression from './components/TemplateExpression';
 import MyComponent from './components/MyComponent';
 import Event from './components/Event';
-// import de imagem
+import Challenge from './components/Challenge';
+
+//import de imagem
 import doguinhos from './assets/doguinhos.jpeg';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Fundamentos do React</p>
-
-      <FirstComponent />
-      <TemplateExpression />
-      <MyComponent />
-      <Event />
+<div>
+  <Event/>
 
       <br />
 
-      <div className="doguinhoscontainer">
+      <h3>Desafio 1:</h3>
+            <div className="doguinhoscontainer">
         <img src={doguinhos} alt="Cachorrinhos salsicha brincando." className="imagem" />
       </div>
+
+      <br/>
+      <h3>Desafio 2:</h3>
+      <Challenge/>
+      <br/>
     </div>
-  );
-}
+
+      
+  
 
 export default App;
